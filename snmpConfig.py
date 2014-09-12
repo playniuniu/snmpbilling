c3bf9894b47b2abc08dbe976f426a9eb7a936a7e
playniuniu <playniuniu@gmail.com>
2014-09-12 11:31:40 +0800

    add new device

--- snmpConfig.py
+++ snmpConfig.py
@@ -6,6 +6,8 @@ snmp_list = [
     {'dev_name': 'ne40e-233', 'ip_addr': '221.192.23.233',  'community': 'luquanne40e12!@', 'user': 'sjz'},
     {'dev_name': 's9312-253', 'ip_addr': '110.249.211.253', 'community': 'luquanne4012!@' , 'user': 'sjz'},
     {'dev_name': 's9312-254', 'ip_addr': '110.249.211.254', 'community': 'luquanne40e12!@', 'user': 'sjz'},
+    {'dev_name': 'esn-1', 'ip_addr': '61.182.128.1', 'community': 'IDCHB&)!*-1SNMPro', 'user': 'sjz'},
+    {'dev_name': 'esn-2', 'ip_addr': '61.182.128.2', 'community': 'IDCHB&)!*-2SNMPro', 'user': 'sjz'},
 ]
 
 snmp_mib = [
